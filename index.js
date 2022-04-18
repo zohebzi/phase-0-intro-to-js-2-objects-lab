85d7be17868efca2fef296f582bf36ee25abede2
Zoheb Iqbal <zoheb.zafar.iqbal@gmail.com>
2022-04-18 16:41:26 -0400

    commit tree a37d1ca840dff86b59fdbbfe10238c9d18d80114

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAdress: '11 Broadway'
+ };
+ 
+ function updateEmployeeWithKeyAndValue(employee, key, value){
+     return Object.assign( {}, employee, { [key]: value})
+ }
+ 
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+     employee[key] = value;
+     return employee
+ }
+ 
+ function deleteFromEmployeeByKey(employee, key) {
+     const newObj = Object.assign({}, employee);
+     delete newObj[key];
+     return newObj
+ }
+ 
+ function destructivelyDeleteFromEmployeeByKey(employee, key){
+     delete employee[key]
+     return employee
+ }
